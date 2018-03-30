bbbc2025a24a927ce6cfd1ce8999ed8e3e593c0e
ProjectSymbol <37919469+ProjectSymbol@users.noreply.github.com>
2018-03-29 21:01:05 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 's"2093847"') {
+    	message.reply('```Hello```');
   	}
 });
 
